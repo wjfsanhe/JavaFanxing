d44588848fd74842d85a8373a39da566263e317c
王建峰 <diegowang@qiyi.com>
2018-01-04 10:45:04 +0800

    add super Generics

--- Test.java
+++ Test.java
@@ -1,7 +1,13 @@
 class Test {
-class Food{};
-class Fruit extends Food{};
-class Apple extends Fruit{};
+class Food{
+public String name = new String("Food");
+};
+class Fruit extends Food{
+public String name = new String("Fruit");
+};
+class Apple extends Fruit{
+public String name = new String("Apple");
+};
 
 static class Plate<T> {
 private T mItem;
@@ -14,8 +20,24 @@ public static void main(String args[]){
 	owner.run("list");
 };
 public void run(String args){
-	Plate<? extends Fruit> plate = new Plate<Apple>(new Apple());
-	//plate.set(new Apple());
-	plate.get();
+	Plate<? extends Food> plate = new Plate<Apple>(new Apple());  // ?  represent compiler didn't know it's type exactly.
+	//plate.set(new Apple());                    Compile Wrong
+	//Apple apple =plate.get();			         Compile Wrong
+	Apple apple =(Apple) plate.get();			 //Right , force convert , so setter must be user. 
+	System.out.println("Food name : " + apple.name); // output Apple
+	
+	Plate<? super Food> plate2 = new Plate<Food> (new Apple());
+	plate2.set(new Apple());
+	plate2.set(new Fruit());  //OK
+	//Apple t = (Apple)plate2.get(); //runtime Wrong , Fruit can't convert to Apple
+	Fruit t = (Fruit)plate2.get(); 
+	
+	System.out.println("Food name : " + t.name); //output :  Fruit
+	
+	plate2.set(new Apple());
+	t = (Fruit)plate2.get(); // ok , Object (Apple) can convert to Fruit. 
+	System.out.println("Food name : " + t.name); //output :  Fruit
+	Apple a = (Apple)plate2.get(); // ok , Object (Apple) can convert to Apple. 
+	System.out.println("Food name : " + a.name); //output :  Apple
 }
 }
